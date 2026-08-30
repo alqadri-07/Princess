aefca27849b5421719aec56ea262bb0d67463eb7
Al Qadri <alqadritaufik3@gmail.com>
2026-08-31 03:44:33 +0700

    Ultah Princess

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Princess/', // WAJIB ada agar path file tidak 404 di GitHub Pages
+  base: '/Princess/', // Sesuaikan dengan nama repository GitHub Anda
   plugins: [
     react(),
     tailwindcss(),
