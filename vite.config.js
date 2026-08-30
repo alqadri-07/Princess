4c8e2a5d01a4e0eac02685e34a14cb693b7f99e3
Al Qadri <alqadritaufik3@gmail.com>
2026-08-31 02:46:04 +0700

    Ultah Princess

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  // Ganti '/nama-repositori-anda/' dengan nama repository GitHub Anda yang sebenarnya.
+  // Contoh: Jika URL GitHub Pages Anda adalah 'username.github.io/birthday-salamah/', 
+  // maka tuliskan '/birthday-salamah/'
+  base: '/nama-repositori-anda/', 
+  
   plugins: [
     react(),
     tailwindcss(),
